e67a2671014aba8557b9f4fe6ad0f4ee6833402c
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2023-02-01 23:56:41 +0300

    Initial commit

---
+++ Lesson1.cs
@@ -0,0 +1,15 @@
+﻿namespace lesson1test 
+{ 
+        internal class Lesson1
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            Console.WriteLine("Результат умножения:");
+            Console.WriteLine(int.Parse(a) * int.Parse(b));
+                    }
+
+
+    }
+}
